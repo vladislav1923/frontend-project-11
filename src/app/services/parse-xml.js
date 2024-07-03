41ab55e530022e9bad445f4c0af606670e320d1c
kozak <kozak@192.168.1.10>
2024-07-03 14:43:57 +0300

    handle invalid rss

--- src/app/services/parse-xml.js
+++ src/app/services/parse-xml.js
@@ -3,7 +3,13 @@ import DomParser from "./dom-parser";
 export default function parseXML(xml, error) {
     return new Promise((res, rej) => {
         try {
-            res(DomParser.parseFromString(xml, 'text/xml'));
+            const dom = DomParser.parseFromString(xml, 'text/xml');
+            const rss = dom.querySelector('rss');
+            if (!rss) {
+                rej(error);
+            } else {
+                res(dom);
+            }
         } catch {
             rej(error);
         }
