e364bacd50870b4ea0370ca7f0db9b3de003230a
kozak <kozak@192.168.1.10>
2024-07-03 15:08:41 +0300

    handle network error

--- src/app/services/fetch-posts.js
+++ src/app/services/fetch-posts.js
@@ -1,5 +1,12 @@
 import { get } from "./http-client";
 
-export default function fetchPosts(url) {
-    return get(url);
+export default function fetchPosts(url, error) {
+    return new Promise(async (res, rej) => {
+        try {
+            const response = await get(url);
+            res(response);
+        } catch {
+            rej(error);
+        }
+    });
 }
